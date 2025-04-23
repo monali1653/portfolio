6794237bd798098049c082422aa8b0fc961b81a1
iammonali <monalisasamal1653@gmail.com>
2025-04-23 17:42:04 +0530

    navbar changes done

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
     <div id="Experience" className="p-10 md:p-24">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Experience</h1>
       <div className="flex flex-wrap items-center justify-around">
-        {/* Left Side: Tech Stack Icons */}
+        
         <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 py-10">
           <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
             <FaHtml5 color="#E34F26" size={50} />
@@ -30,9 +30,9 @@ const Experience = () => {
           </span>
         </div>
 
-        {/* Right Side: Work Experience */}
+        
         <div className="flex gap-10 bg-slate-950 bg-opacity-45 mt-4 rounded-lg p-4 items-center">
-          <img src="/src/images/ubslogo-removebg-preview.png" alt="UBS logo" className="w-14 h-14 object-contain" />
+          <img src="/images/ubslogo-removebg-preview.png" alt="UBS logo" className="w-14 h-14 object-contain" />
           <span className="text-white">
             <h2 className="leading-tight">Hackathon Participant, UBS</h2>
             <p className="text-sm leading-tight font-thin">3 April, 2025 - 4 April, 2025</p>
